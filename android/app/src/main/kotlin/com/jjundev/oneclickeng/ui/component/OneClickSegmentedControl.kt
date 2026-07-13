c88e386d3e40ddbdb307161882ea7ff1d0d95c34
jjundev <johnshin617@gmail.com>
2026-07-13 23:09:09 +0900

    chore(segmented): remove now-unused dp import

--- android/app/src/main/kotlin/com/jjundev/oneclickeng/ui/component/OneClickSegmentedControl.kt
+++ android/app/src/main/kotlin/com/jjundev/oneclickeng/ui/component/OneClickSegmentedControl.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.jjundev.oneclickeng.ui.theme.OceTheme
 
 /**
